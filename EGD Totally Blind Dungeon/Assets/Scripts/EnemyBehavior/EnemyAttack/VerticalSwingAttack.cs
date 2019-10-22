390808acbd67e56368972875ca58a1e78ba741a5
sophierichards <richas3@rpi.edu>
2019-10-21 23:52:03 -0400

    added a vertical swing attack

--- EGD Totally Blind Dungeon/Assets/Scripts/EnemyBehavior/EnemyAttack/VerticalSwingAttack.cs
+++ EGD Totally Blind Dungeon/Assets/Scripts/EnemyBehavior/EnemyAttack/VerticalSwingAttack.cs
@@ -2,26 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BasicSwingAttack : EnemyAttack
+public class VerticalSwingAttack : EnemyAttack
 {
-    private void Start() {
-        StartAttack();
-    }
     public float angle = 45f;
     public float attackSpeed = 100f;
-    public bool isClockwise = false;
+    public bool startOnBottom = false;
     int direction = 1;
     public bool startInFront = false;
     public override void StartAttack(){
         attackCompleted = false;
-        if(isClockwise){
+        if(startOnBottom){
             direction = 1;
         }
         else{
             direction = -1;
         }
         weapon.transform.rotation = transform.rotation;
-        if(!startInFront) weapon.transform.RotateAround(transform.position, transform.up, angle*direction*-1);
+        if(!startInFront) weapon.transform.RotateAround(transform.position, transform.right, angle*direction*-1);
         StartCoroutine("SwingSword");
     }
     public override void InterruptAttack(){
@@ -32,10 +29,11 @@ public class BasicSwingAttack : EnemyAttack
         attackCompleted = true;
         float currentAngle = 2*angle;
         while(currentAngle > 0){
-            weapon.transform.RotateAround(transform.position, transform.up, attackSpeed*direction*Time.deltaTime);
+            weapon.transform.RotateAround(transform.position, transform.right, attackSpeed*direction*Time.deltaTime);
             currentAngle-=attackSpeed*Time.deltaTime;
             yield return null;
         }
         attackCompleted = true;
     }
+
 }
