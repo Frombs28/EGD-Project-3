714c191c52d559f8b717d61a959d1342912af2d4
sophierichards <richas3@rpi.edu>
2019-10-28 22:40:16 -0400

    created a dummy FSM for cube moving for reference

---
+++ EGD Totally Blind Dungeon/Assets/Scripts/StateMachineFramework/TransitionsAndStates/MoveAwayFromPlayerState.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAwayFromPlayerState : State
+{
+    public override void DoEntryAction(AIController ai){
+    }
+    public override void DoExitAction(AIController ai){
+    }
+
+    public override void DoAction(AIController ai){
+        ai.MoveAwayFromPlayer();
+    }
+}
---
+++ EGD Totally Blind Dungeon/Assets/Scripts/StateMachineFramework/TransitionsAndStates/MoveTowardPlayerState.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowardPlayerState : State
+{
+    public override void DoEntryAction(AIController ai){
+    }
+    public override void DoExitAction(AIController ai){
+    }
+
+    public override void DoAction(AIController ai){
+        ai.MoveTowardPlayer();
+    }
+}
