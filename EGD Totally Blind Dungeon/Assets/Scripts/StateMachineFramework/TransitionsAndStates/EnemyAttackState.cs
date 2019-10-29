1a5ab4e26bf7da8f3e991bf557adc586c002c44f
sophierichards <richas3@rpi.edu>
2019-10-28 21:47:28 -0400

    created a enemy attack state and transition

---
+++ EGD Totally Blind Dungeon/Assets/Scripts/StateMachineFramework/TransitionsAndStates/EnemyAttackState.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttackState : State
+{
+    public EnemyAttack ea;
+    public override void DoEntryAction(){
+        ea.StartAttack();
+    }
+    public override void DoExitAction(){
+    }
+
+    public override void DoAction(){
+
+    }
+
+}
---
+++ EGD Totally Blind Dungeon/Assets/Scripts/StateMachineFramework/TransitionsAndStates/EnemyAttackTransition.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttackTransition : Transition
+{
+    public EnemyAttack ea;
+    public override bool IsTriggered(){
+        return ea.IsAttackDone();
+    }
+    public override void DoAction(){
+        
+    }
+}
