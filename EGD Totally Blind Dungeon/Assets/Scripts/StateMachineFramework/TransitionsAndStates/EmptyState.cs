e5c9a268d2212569b263e1454fa5109bcc0c7374
sophierichards <richas3@rpi.edu>
2019-10-29 21:18:27 -0400

    created a generic empty state

---
+++ EGD Totally Blind Dungeon/Assets/Scripts/StateMachineFramework/TransitionsAndStates/EmptyState.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmptyState : State
+{
+    public override void DoAction(AIController ai){}
+    public override void DoEntryAction(AIController ai){}
+    public override void DoExitAction(AIController ai){}
+}
