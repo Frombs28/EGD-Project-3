b628850eb4006e58d34f2dd1705e481966123ac1
Unknown <castrj5@rpi.edu>
2019-11-02 15:56:07 -0400

    adding reverb zones

---
+++ EGD Totally Blind Dungeon/Assets/BetterReverbZone.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class BetterReverbZone : MonoBehaviour
+{
+    public AudioMixerGroup reverbChannel;
+    [Range(-10000,0)]
+    public float room = -10000f;
+    [Range(0.1f, 20f)]
+    public float decayTime = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+}
