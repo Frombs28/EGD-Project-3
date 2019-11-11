99a5ee130ca1402ee82822405af6c7d350196841
sophierichards <richas3@rpi.edu>
2019-11-10 21:56:47 -0500

    Added static hitbox attack

---
+++ EGD Totally Blind Dungeon/Assets/Scripts/EnemyBehavior/EnemyAttack/StaticHitboxAttack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticHitboxAttack : EnemyAttack
+{
+    public BoxCollider hitbox;
+    public float timeToAttack = 2f;
+    public float attackActiveTime = 2f;
+    void Start(){
+        hitbox.enabled = false;
+    }
+    public override void StartAttack(){
+        attackCompleted = false;
+        StartCoroutine("SpawnHitbox");
+    }
+    public override void InterruptAttack(){
+        StopAllCoroutines();
+        hitbox.enabled = false;
+        attackCompleted = true;
+    }
+    IEnumerator SpawnHitbox(){
+        //maybe switch below to a while loop for sound timing
+        yield return new WaitForSeconds(timeToAttack);
+        hitbox.enabled = true;
+        yield return new WaitForSeconds(attackActiveTime);
+        hitbox.enabled = false;
+        attackCompleted = true;
+    }
+}
