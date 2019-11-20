e85f0b022b66f6a8be29ba120144921a72b6bd93
Steve <frombs@rpi.edu>
2019-11-19 22:13:52 -0500

    Pockets interact with item tracker

--- EGD Totally Blind Dungeon/Assets/Scripts/Pocket.cs
+++ EGD Totally Blind Dungeon/Assets/Scripts/Pocket.cs
@@ -6,10 +6,22 @@ public class Pocket : MonoBehaviour
 {
     bool full;
     public Interact curItem = null;
+    public ItemTracker tracker;
+    public int id;
+    /*
+      Each pocket has an id number:
+      0 = Back
+      1 = Top left
+      2 = Top right
+      3 = Bottom left
+      4 = Bottom right
+    */
+
     // Start is called before the first frame update
     void Start()
     {
         full = false;
+        tracker = FindObjectOfType<ItemTracker>();
     }
 
     // Update is called once per frame
@@ -31,6 +43,7 @@ public class Pocket : MonoBehaviour
         item.transform.localPosition = Vector3.zero;
         curItem.GetComponent<Rigidbody>().isKinematic = false;
         curItem.GetComponent<Rigidbody>().useGravity = false;
+        tracker.NewPocketItem(curItem,id);
     }
 
     public void Empty()
@@ -40,5 +53,6 @@ public class Pocket : MonoBehaviour
         curItem.GetComponent<Rigidbody>().isKinematic = true;
         curItem.GetComponent<Rigidbody>().useGravity = true;
         curItem = null;
+        tracker.RemovePocketItem(id);
     }
 }
