734914de8a6cb89582ac61f3d33f3144327bb582
Steve <frombs@rpi.edu>
2019-11-19 21:29:36 -0500

    Pocket code; needs proper testing

---
+++ EGD Totally Blind Dungeon/Assets/Scripts/Pocket.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pocket : MonoBehaviour
+{
+    bool full;
+    public Interact curItem = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        full = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool IsFull()
+    {
+        return full;
+    }
+
+    public void Fill(Interact item)
+    {
+        full = true;
+        curItem = item;
+        item.transform.parent = gameObject.transform;
+        item.transform.localPosition = Vector3.zero;
+        curItem.GetComponent<Rigidbody>().isKinematic = false;
+        curItem.GetComponent<Rigidbody>().useGravity = false;
+    }
+
+    public void Empty()
+    {
+        full = false;
+        curItem.transform.parent = null;
+        curItem.GetComponent<Rigidbody>().isKinematic = true;
+        curItem.GetComponent<Rigidbody>().useGravity = true;
+        curItem = null;
+    }
+}
